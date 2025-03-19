418694c0e78bc87ec9ef568506d055a15d5268c7
MohamadAlaskari <m_alskari96@hotmail.com>
2025-03-19 23:26:37 +0100

    creat res auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { DatabaseModule } from './database/database.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { UsersModule } from './modules/users/users.module';
 import { CategoriesModule } from './modules/categories/categories.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ProjectsModule, UsersModule, CategoriesModule],
+  imports: [DatabaseModule, ProjectsModule, UsersModule, CategoriesModule, AuthModule],
   controllers: [],
   providers: [],
 })
--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,23 +1,37 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Neues Projekt erstellen' })
+  @ApiResponse({ status: 201, description: 'Projekt erfolgreich erstellt' })
   create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectsService.create(createProjectDto);
   }
-
   @Get()
+  @ApiOperation({ summary: 'Alle Projekte anzeigen' })
+  @ApiResponse({ status: 200, description: 'Liste aller Projekte' })
   findAll() {
     return this.projectsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Ein Projekt anhand der ID anzeigen' })
+  @ApiResponse({ status: 200, description: 'Projekt gefunden' })
   findOne(@Param('id') id: string) {
     return this.projectsService.findOne(+id);
   }
