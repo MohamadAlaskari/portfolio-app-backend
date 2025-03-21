8b33b4af101f8a425b351a3b72103556b7cf8bc4
MohamadAlaskari <m_alskari96@hotmail.com>
2025-03-21 21:26:41 +0100

    install npm install --save @nestjs/serve-static for swagger deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { UsersModule } from './modules/users/users.module';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-static'),
+      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+    }),
     ConfigModule.forRoot({
       envFilePath: ['.env', '.env.development'],
       isGlobal: true,
